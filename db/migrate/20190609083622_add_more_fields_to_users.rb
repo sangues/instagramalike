d54375c46f02d1b9cba159d5b5eb23a370ea82de
Sangeeth Sudhakar <sangeeth@getsimpl.com>
2019-06-09 20:11:27 +0530

    initial commit
    - add users controller
    - add users model
    - add basic sign-in

---
+++ db/migrate/20190609083622_add_more_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddMoreFieldsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :name, :string
+    add_column :users, :bio, :string
+    add_column :users, :phone, :integer
+    add_column :users, :gender, :string
+  end
+end
